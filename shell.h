b1ea1333bb2231f08ee24bf411a63a6a2d9b841a
Iho Chike <ihosidney@gmail.com>
2022-05-18 08:01:20 +0000

    main

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -77,4 +77,4 @@ void print_error(vars_t *vars, char *msg);
 void _puts2(char *str);
 char *_uitoa(unsigned int count);
 
-#endif /* _MAIN_H_ */
+#endif /* _SHELL_H_ */
